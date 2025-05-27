d202d535234847bc36bf82498fd1ca2adeede14b
Colton Wollinger <cwoll1home@gmail.com>
2025-05-27 19:58:55 -0400

    Most violet stuff works again, still gotta add the stun meter thingy tho

--- shared/kuro_component.gd
+++ shared/kuro_component.gd
@@ -4,11 +4,10 @@ class_name KURO_Component
 const YOURE_NOT_PART_OF_ONE = null
 
 func wait_for_component(component_name):
-	print("[Globals.gd] Waiting for %s" % component_name)
+	print("[%s] (BASE) Waiting for component" % [self.name])
 	while component_name not in self.exports:
-		pass
-	
-	print("[Globals.gd] Done waiting for %s" % component_name)
+		await get_tree().process_frame
+	return self.exports.get(component_name)
 
 func get_scene_root(me_myself_and_i):
 	var current = me_myself_and_i
@@ -17,9 +16,9 @@ func get_scene_root(me_myself_and_i):
 	return current
 
 func WhatIsTheRootComponent(me_myself_and_i):
-	print("[Globals.gd] Finding root component")
+	print("[%s] (BASE) Finding root component" % [self.name])
 	var export = get_scene_root(me_myself_and_i)
-	print("[Globals.gd] Got %s" % export)
+	print("[%s] (BASE) Got %s" % [self.name, export])
 	return export  # null is checkable as YOURE_NOT_PART_OF_ONE too
 
 func ExportUnder(parent, me_myself_and_i, name=null):
@@ -29,7 +28,7 @@ func ExportUnder(parent, me_myself_and_i, name=null):
 	if name == "":
 		name = me_myself_and_i.name
 	
-	print("[Globals.gd] Exporting... %s  UNDER %s AS %s" % [me_myself_and_i.name, parent.name, name])
+	print("[%s] (BASE) Exporting... %s UNDER %s AS %s" % [self.name, me_myself_and_i.name, parent.name, name])
 	parent.exports.set(name, me_myself_and_i)
 
 
